848eb7d06c2bfc57c9b0f70042b624bc013b80ea
Jack W. Friend <jackwillamfriend@gmail.com>
2015-07-16 14:23:30 -0400

    First commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # sketchpad
 
 http://www.theodinproject.com/web-development-101/javascript-and-jquery?ref=lnav
+
+This project is a bit *sketchy.*
+Yeah, its not really good, just kinda did it quickly,
+there are quite a few bugs
---
+++ functions.js
@@ -0,0 +1,40 @@
+$(document).ready( function(){
+
+  // when user clicks on the button
+  $('button').click(function(){
+
+    // remove all div's
+    $('div').remove();
+    var ans = prompt("How big do you want the grid to be?");
+
+    // to make the divs
+    for (var i = 0; i < ans * ans; i++){
+      $('body').append('<div></div>');
+    }
+
+    // to find the size of each div
+    var boxSize = Math.floor(464 / ans) + 'px';
+    boxSize.toString();
+    $('div').css({
+      'width': boxSize,
+      'height': boxSize,
+    });
+
+    // when hovering over a div
+
+    function getRandomColor() {
+      var letters = '0123456789ABCDEF'.split('');
+      var color = '#';
+      for (var i = 0; i < 6; i++ ) {
+          color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+
+    $('div').hover(function(){
+      $(this).css({
+        'background-color': getRandomColor()
+      });
+    });
+  });
+});
